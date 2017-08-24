47b8dd19b5cf791e6084291992773c003cce66bc
Jon Callahan <jon_callahan@intuit.com>
2017-08-24 14:17:04 -0500

    opens links too

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function openPageInOutline(event) {
-  const url = `https://outline.com/${event.pageUrl}`;
+  const target = event.linkUrl || event.pageUrl;
+  const url = `https://outline.com/${target}`;
   chrome.tabs.create({url});
 }
 chrome.contextMenus.create({
   title: 'Outline this page',
-  contexts: ['page'],
+  contexts: ['page', 'link'],
   onclick: openPageInOutline,
   type: 'normal',
 });
